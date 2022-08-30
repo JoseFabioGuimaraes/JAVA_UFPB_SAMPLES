8dc00cedd525837c557225fe0dea605b06c83e3b
José Fábio Guimarães de Assis Filho <71357050+JoseFabioGuimaraes@users.noreply.github.com>
2022-08-30 10:50:49 -0300

    Add files via upload

---
+++ helloWorld.java
@@ -0,0 +1,21 @@
+///IMC CALCULADORA
+import javax.swing.JOptionPane;
+public class helloWorld {
+
+    public static void main(String [] args){
+        String nome = JOptionPane.showInputDialog("Digite seu nome");
+        //int idade = Integer.parseInt(JOptionPane.showInputDialog("Digite sua idade: "));
+        double altura = Double.parseDouble(JOptionPane.showInputDialog("Digite sua Altura"));
+        double peso = Double.parseDouble(JOptionPane.showInputDialog("Digite seu Peso"));
+        double CalculoIMC = peso/(altura*altura);
+        JOptionPane.showMessageDialog( null , nome +String.format(" Seu imc é: %.1f",CalculoIMC));
+
+        if (CalculoIMC <18.00){
+            JOptionPane.showMessageDialog(null,"Você está abaixo do peso ideal");
+        } else if(CalculoIMC >= 18 && CalculoIMC <=24.9){
+            JOptionPane.showMessageDialog(null,"Você está no peso ideal");
+        } else{
+            JOptionPane.showMessageDialog(null,"Você está acima do peso");
+        }
+    }
+}
